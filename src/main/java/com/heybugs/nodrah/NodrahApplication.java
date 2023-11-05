6d8edf892194f36e91456cf26ea280b3782a01d7
itisnevertoolat <muhammedmady100@gmail.com>
2023-11-05 21:49:57 +0200

    our first commit for this easy peasy project

---
+++ src/main/java/com/heybugs/nodrah/NodrahApplication.java
@@ -0,0 +1,13 @@
+package com.heybugs.nodrah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodrahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodrahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heybugs/nodrah/NodrahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heybugs.nodrah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodrahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
